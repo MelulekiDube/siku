69dfbb8531c8b0de5a4e2a09cc525815f6223e4a
Meluleki <mellarmellah@gmail.com>
2019-02-16 18:48:31 +0200

    Designed the interface and wrote the overall layout for the TrayIcon

---
+++ src/SikuTrayIcon.java
@@ -0,0 +1,106 @@
+
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dube_
+ */
+public class SikuTrayIcon {
+
+    private static final String ICON_FILE_NAME = "res/time.png";
+    
+    private final TrayIcon trayicon;
+    private final SystemTray systemTray;
+
+    
+    public SikuTrayIcon() {
+	trayicon = new TrayIcon(createImage(ICON_FILE_NAME), "Siku");
+	systemTray = testAndReturnTray();
+    }
+    
+    private void init(){
+	trayicon.addMouseListener(init_mouseListener());
+	trayicon.addActionListener(init_actionListener());
+	try {
+	    systemTray.add(trayicon);
+	} catch (AWTException ex) {
+	    Logger.getLogger(SikuTrayIcon.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }    
+    
+    private MouseListener init_mouseListener(){
+	return new MouseListener() {
+	    
+	    @Override
+	    public void mouseClicked(MouseEvent e) {
+		//we will open the interface here.
+	    }
+
+	    @Override
+	    public void mousePressed(MouseEvent e) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	    }
+
+	    @Override
+	    public void mouseReleased(MouseEvent e) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	    }
+
+	    @Override
+	    public void mouseEntered(MouseEvent e) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	    }
+
+	    @Override
+	    public void mouseExited(MouseEvent e) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	    }
+	};
+    }
+    
+    private ActionListener init_actionListener(){
+	return new ActionListenerImpl();
+    }
+    
+    private SystemTray testAndReturnTray() {
+	if(SystemTray.isSupported()){
+	    return SystemTray.getSystemTray();
+	}
+	JOptionPane.showMessageDialog(null, "FInale not there", "Error obtaining System tray", JOptionPane.ERROR_MESSAGE);
+	return null;
+    }
+    
+    private Image createImage(String s) {
+	Toolkit toolkit = Toolkit.getDefaultToolkit();
+	return toolkit.getImage(s);
+    }
+
+    private static class ActionListenerImpl implements ActionListener {
+
+	public ActionListenerImpl() {
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+    }
+
+}
